5d7c03ce4ccb6effa0a04045dee7e7161883856a
ryota-take <77515214+ryota-take@users.noreply.github.com>
2021-05-01 07:13:58 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@
 
 {
   const nav_menu = document.querySelectorAll('.nav-menu > ul > li');
+  const nav_img = document.querySelectorAll('.nav-menu > ul > li > img');
   const nav_menu_0 = document.querySelector('.nav-menu-0');
   const nav_menu_1 = document.querySelector('.nav-menu-1');
   const nav_menu_2 = document.querySelector('.nav-menu-2');
@@ -26,12 +27,17 @@
   nav_menu[0].addEventListener('click', ()=> {
     nav_menu_0.classList.toggle('show');
     nav_menu[0].classList.toggle('color');
+    nav_img[0].classList.toggle('img_hidden');
 
     nav_menu_1.classList.remove('show');
     nav_menu[1].classList.remove('color');
+    nav_img[1].classList.remove('img_hidden');
+
 
     nav_menu_2.classList.remove('show');
     nav_menu[2].classList.remove('color');
+    nav_img[2].classList.remove('img_hidden');
+
 
   } );
  
@@ -40,24 +46,35 @@
   nav_menu[1].addEventListener('click', ()=> {
     nav_menu_1.classList.toggle('show');
     nav_menu[1].classList.toggle('color');
+    nav_img[1].classList.toggle('img_hidden');
 
     nav_menu_0.classList.remove('show');
     nav_menu[0].classList.remove('color');
+    nav_img[0].classList.remove('img_hidden');
+
 
     nav_menu_2.classList.remove('show');
     nav_menu[2].classList.remove('color');
+    nav_img[2].classList.remove('img_hidden');
+
   } );
  
 
   nav_menu[2].addEventListener('click', ()=> {
     nav_menu_2.classList.toggle('show');
     nav_menu[2].classList.toggle('color');
+    nav_img[2].classList.toggle('img_hidden');
+
 
     nav_menu_1.classList.remove('show');
     nav_menu[1].classList.remove('color');
+    nav_img[1].classList.remove('img_hidden');
+
 
     nav_menu_0.classList.remove('show');
     nav_menu[0].classList.remove('color');
+    nav_img[0].classList.remove('img_hidden');
+
 
   } );
  
